17478ea6c43d0fa363e4f66e4f48109b21f1d5c4
ImtiazAhmed01 <imtiazahm21@gmail.com>
2025-02-05 12:11:04 +0600

    error fixed

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -8,13 +8,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyC_KP7RX06HOs13KvLpwGybAzA6JRTqnnw",
-    authDomain: "lingo-bingo-37a0b.firebaseapp.com",
-    projectId: "lingo-bingo-37a0b",
-    storageBucket: "lingo-bingo-37a0b.firebasestorage.app",
-    messagingSenderId: "143984257672",
-    appId: "1:143984257672:web:96c66af92efddbda884d3d",
-    measurementId: "G-VH84BRVY3F"
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId,
+    measurementId: import.meta.env.VITE_measurementId,
 };
 
 // Initialize Firebase
